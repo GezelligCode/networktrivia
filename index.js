a1cfe3bf256d612468e6444ea0ace5cc775756b2
S.Anspaugh <octopodecode@gmail.com>
2022-02-27 20:14:45 -0800

    Node Server Setup
    Added Node Express server for Heroku deployment

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
